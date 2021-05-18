e809712017df3a7ca3e53e79b67a2bfe6dcf43df
Caleb Waymeyer <cjwaymeyer@gmail.com>
2021-05-18 14:45:11 -0400

    red-blue

--- script.js
+++ script.js
@@ -3,5 +3,5 @@
 document.addEventListener('mousemove', function (event) {
   let g = Math.round((event.pageX * 255) / window.innerWidth);
   let b = Math.round((event.pageY * 255) / window.innerHeight);
-  document.body.style.backgroundColor = `rgb(0, ${g}, ${b})`;
+  document.body.style.backgroundColor = `rgb(${g}, 0, ${b})`;
 });
