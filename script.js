6ce979f82bd5ff9560c57c5d7a02b5933440434c
Caleb Waymeyer <cjwaymeyer@gmail.com>
2021-05-18 14:37:22 -0400

    mouse color map

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+document.addEventListener('mousemove', function (event) {
+  let g = Math.round((event.pageX * 255) / window.innerWidth);
+  let b = Math.round((event.pageY * 255) / window.innerHeight);
+  document.body.style.backgroundColor = `rgb(0, ${g}, ${b})`;
+});
